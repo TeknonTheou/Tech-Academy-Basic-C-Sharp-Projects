533d9a65a81ec49d21bd09febc48a4b7742980bd
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-06-09 16:30:14 -0500

    Submit CallingMethods

---
+++ CallingMethods/CallingMethods/Number.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Number
+    {
+        public int addFive(int num)//define addFive method
+        {
+            return num + 5;//add 5
+        }
+        public int timesFive(int num)//define timesFive method
+        {
+            return num * 5;//multiply by 5
+        }
+        public int divTwo(int num)//define divTwo method
+        {
+            return num / 2;//divide by 2
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            Console.WriteLine("Please enter a whole number.");//request number from user
+            int choice = Convert.ToInt32(Console.ReadLine());//read input and convert to integer
+            Console.WriteLine("Your number + 5 = " + number.addFive(choice) + ".");//run addFive method (adds 5)
+            Console.WriteLine("Your number multiplied by 5 = " + number.timesFive(choice) + ".");//run timesFive method (multiplies by 5)
+            Console.WriteLine("Your number divided by 2 = " + number.divTwo(choice) + ".");//run divTwo method (divides by 2)
+            Console.ReadLine();
+        }
+    }
+}
