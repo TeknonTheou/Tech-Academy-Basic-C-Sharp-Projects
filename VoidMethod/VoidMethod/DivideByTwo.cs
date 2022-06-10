676a72c31f2a48dfbde291582250b6847fe38390
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-06-10 14:04:47 -0500

    Submit VoidMethod

---
+++ VoidMethod/VoidMethod/DivideByTwo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class DivideByTwo
+    {
+        public void Divide(int first, int second)
+        {
+            int number = first / 2;//divide first integer by 2
+            Console.WriteLine("Your second number is: " + second + ".");//display second integer to screen
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DivideByTwo divide = new DivideByTwo();
+            int firstAnswer = 0;
+            int secondAnswer = 0;
+            Console.WriteLine("Input a whole number to be divided by two: ");//request whole number (note...result of operation  not displayed on screen. Display not noted in assignment requirements)
+            firstAnswer = Convert.ToInt32(Console.ReadLine());//convert input to integer
+            Console.WriteLine("Input a second whole number to display on the screen");//request whole number
+            secondAnswer = Convert.ToInt32(Console.ReadLine());//convert input to integer
+            divide.Divide(firstAnswer, secondAnswer);//call method passing input numbers
+            divide.Divide(first: 6, second: 2);//call method, specifying parameters by name
+            Console.ReadLine();
+        }
+    }
+}
