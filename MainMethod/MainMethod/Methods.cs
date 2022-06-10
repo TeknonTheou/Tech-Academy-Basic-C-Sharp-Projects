30e6021d1fcdf620d8f4e51bb0963fce9f4d2baa
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-06-09 20:01:12 -0500

    Submit MainMethod

---
+++ MainMethod/MainMethod/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    public class Methods
+    {
+        public int mathOp(int num)//define mathOp method
+        {
+            return num + 5;//add 5
+        }
+        public int mathOp(decimal num)//define mathOp method
+        {
+            decimal num1 = num *5;//multiply by 5
+            int num2 = Convert.ToInt32(num1);//convert to integer
+            return num2;//return answer
+        }
+        public int mathOp(string num)//define mathOp method
+        { 
+            int number = Convert.ToInt32(num);//convert to integer
+            return number + 10;//multiply by 10
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods methods = new Methods();
+            Console.WriteLine("Please enter a whole number");//request number
+            int choice = Convert.ToInt32(Console.ReadLine());//convert to integer
+            Console.WriteLine(methods.mathOp(choice));//print results of method
+            Console.WriteLine("Please enter a decimal number");//request decimal
+            decimal choice1 = Convert.ToDecimal(Console.ReadLine());//convert to decimal
+            Console.WriteLine(methods.mathOp(choice1));//print results of method
+            Console.WriteLine("Please enter a whole number");//request number (all input is considered string)
+            string choice2 = Console.ReadLine();//read input (does not convert)
+            try
+            {
+                Console.WriteLine(methods.mathOp(choice2));//print results of method
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Item entered does not convert to a whole number.");//print if incorrect format entered.
+            }
+            Console.ReadLine();
+        }   
+    }
+}
