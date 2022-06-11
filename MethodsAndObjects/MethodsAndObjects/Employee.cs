df0537b8d3b1d7d4b601f37bce33b128e76d1521
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-06-11 17:26:42 -0500

    Submit MethodsAndObjects

---
+++ MethodsAndObjects/MethodsAndObjects/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Employee : Person
+    {
+        public int ID { get; set; }
+
+        public static bool operator ==(Employee employee1, Employee employee2)//checks IDs to determine if same or different
+        {
+            if (employee1.ID == employee2.ID)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Employee employee1, Employee employee2)//C# requires matching operator to be defined.
+        {
+            if (employee1.ID != employee2.ID)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee sample = new Employee() { FirstName = "Sample", LastName = "Student", ID = 1 };//create instance of Employee Object
+
+            Employee different = new Employee() { FirstName = "William", LastName = "McMillen", ID = 2 };//create a second instance (to demonstrate ID info being taken in)
+
+            sample.SayName();//run 'SayName' method on first employee
+            different.SayName();//run 'SayName' method on second employee
+
+            Console.WriteLine("Are the employee IDs the same? {0}.", sample == different);//Employee class takes ID separately from "person" class, entered method to verify ID is same/different
+
+
+            Console.ReadLine();
+        }
+    }
+}
