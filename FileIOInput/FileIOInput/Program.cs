83c7d7e49f957b87fae4a85b87e9c967b556fcce
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-06-14 19:33:47 -0500

    submit FileIOInput

---
+++ FileIOInput/FileIOInput/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please input a whole number.");//request a number
+            bool validInput = false;//sets default boolean to false
+            string input = "";//instantiates string variable "input"
+            while (!validInput)//loop through try/catch until validInput is true
+            {
+                try
+                {
+                    input = Console.ReadLine();//read user input
+                    Convert.ToInt32(input);//convert user input to integer if possible
+                    validInput = true;//change validInput to true if able to convert
+                }
+                catch
+                {
+                    Console.WriteLine("Whole numbers only!");//request a whole number again
+                    validInput = false;//keeps validInput as false
+                }
+            }
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Tekno\source\repos\Tech-Academy-Basic-C-Sharp-Projects\FileIOInput\number.txt", false))
+            {
+                file.WriteLine(input);//write input to file listed above
+            }
+
+            string fileText = File.ReadAllText(@"C:\Users\Tekno\source\repos\Tech-Academy-Basic-C-Sharp-Projects\FileIOInput\number.txt");
+            Console.WriteLine("The number you entered was {0}", fileText);//print number from file listed above
+            Console.ReadLine();
+        }
+    }
+}
