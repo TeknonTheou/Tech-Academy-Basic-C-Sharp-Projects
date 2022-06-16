b93f162cb9721956cc61b6b91985b1d17e6121f0
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-06-16 11:28:58 -0500

    Submit constructor assignment

---
+++ ConstructorSubmission/ConstructorSubmission/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorSubmission
+{
+    class Animal
+    {
+        public Animal(string name) : this(name, 4) { }//set Animal class default with 4 legs
+
+        public Animal(string name, int numLegs)//set Animal to allow user to input name and number of legs
+        {
+            Name = name;
+            NumLegs = numLegs;
+        }
+
+        public string Name { get; set; }
+        public int NumLegs { get; set; }
+    }
+}
---
+++ ConstructorSubmission/ConstructorSubmission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConstructorSubmission
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string storeName = "Billy's Pet Shop";//create const variable for store name
+            Animal bird = new Animal("Polly the parrot", 2);//build new Animal
+            Animal dog = new Animal("Bailey the beagle");//build second animal
+            var animals = new List<Animal>() { bird, dog };//create list of animals under "var"
+
+
+            Console.WriteLine("Welcome to {0}, This is our current inventory.", storeName);//display const variable
+            Console.WriteLine("Animals:");
+            foreach (Animal animal in animals)//write each animal in list to console.
+            {
+                Console.WriteLine("Name: {0}, Number of legs: {1}", animal.Name, animal.NumLegs);
+            }
+
+            Console.WriteLine("Wow, we could really use some more animals...");
+            Console.ReadLine();
+        }
+    }
+}
