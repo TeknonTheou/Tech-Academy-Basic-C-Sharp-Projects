8140ac671f81f111a35e872a1ee18ea0db320720
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-06-21 19:02:31 -0500

    Commit CarInsurance app

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (var db = new InsuranceEntities())
+            {
+                var quotes = db.Insurees;
+                var quoteVms = new List<QuoteVm>();
+                foreach (var quote in quotes)
+                {
+                    var quoteVm = new QuoteVm();
+                    quoteVm.FirstName = quote.FirstName;
+                    quoteVm.LastName = quote.LastName;
+                    quoteVm.EmailAddress = quote.Email_Address;
+                    quoteVm.QuoteAmount = (decimal)quote.Quote;
+
+                    quoteVms.Add(quoteVm);
+                }
+                return View(quoteVms);
+            }
+        }
+    }
+}
---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendForm(string firstName, string lastName, string emailAddress, DateTime dateOfBirth, int carYear, string carMake, string carModel, bool dui, int speedTickets, string coverage)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(emailAddress) || dateOfBirth == null || carYear == 0 || string.IsNullOrEmpty(carMake) || string.IsNullOrEmpty(carModel) || string.IsNullOrEmpty(coverage))
+            {
+                return View("~/Views/Shared/Error.cshtml");
+            }
+
+            decimal quoteAmount = 50;
+
+            using (var db = new InsuranceEntities())
+            {
+                var quote = new Insuree();
+
+
+                if (DateTime.Now - dateOfBirth < TimeSpan.FromDays(365 * 25))// if user is under 25
+                {
+                    quoteAmount += 25;
+                }
+                if (DateTime.Now - dateOfBirth < TimeSpan.FromDays(365 * 18))// if user is under 18
+                {
+                    quoteAmount += 100;
+                }
+                else if (DateTime.Now - dateOfBirth > TimeSpan.FromDays(365 * 100))// if user is over 100
+                {
+                    quoteAmount += 25;
+                }
+
+                if (carYear < 2000)
+                {
+                    quoteAmount += 25;
+                }
+                else if (carYear > 2015)
+                {
+                    quoteAmount += 25;
+                }
+
+                if (carMake.ToLower().Contains("porsche"))
+                {
+                    quoteAmount += 25;
+
+                    if (carModel.ToLower().Contains("911 carrera"))
+                    {
+                        quoteAmount += 25;
+                    }
+                }
+
+                quoteAmount += speedTickets * 10;
+
+                if (dui)
+                {
+                    quoteAmount *= (decimal)1.25;
+                }
+
+                bool fullCoverage = (coverage == "Full");
+
+                if (fullCoverage)
+                {
+                    quoteAmount *= (decimal)1.5;
+                }
+
+                quote.FirstName = firstName;
+                quote.LastName = lastName;
+                quote.Email_Address = emailAddress;
+                quote.DateOfBirth = dateOfBirth;
+                quote.CarYear = carYear;
+                quote.CarMake = carMake;
+                quote.CarModel = carModel;
+                quote.DUI = dui;
+                quote.SpeedingTickets = speedTickets;
+                quote.Coverage_Type = fullCoverage;
+                quote.Quote = quoteAmount;
+
+                db.Insurees.Add(quote);
+                db.SaveChanges();
+            }
+
+            return View("Quote", quoteAmount);
+        }
+    }
+}
