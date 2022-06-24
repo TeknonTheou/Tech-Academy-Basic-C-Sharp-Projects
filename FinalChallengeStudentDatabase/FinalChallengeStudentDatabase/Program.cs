3b45d0f4c29a1b24f34ad67f55b4a212ba19f4d2
TeknonTheou <99988665+TeknonTheou@users.noreply.github.com>
2022-06-24 11:42:03 -0500

    Commit CodeFirst Challenge

---
+++ FinalChallengeStudentDatabase/FinalChallengeStudentDatabase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections;
+
+
+namespace FinalChallengeStudentDatabase
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Billy McMillen" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.Write("Student saved successfully!");
+
+                var query = from i in ctx.Students
+                          orderby i.StudentName
+                          select i;
+
+                Console.WriteLine("All students in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.StudentName);
+                }
+
+
+            }
+        }
+    }
+
+}
